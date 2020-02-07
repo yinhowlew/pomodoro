bf7bef0492cce82f05e733a8932b97c3b1ebf95c
yinhow <yinhowlew@gmail.com>
2020-02-07 18:19:27 -0500

    Updates

---
+++ precache-manifest.d7e00dbcaa9ec2d8419c3643e1459203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053449876da49b2bd4c1c5fe358335d0",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "722810dbd8b844450306",
+    "url": "/pomodoro/static/css/main.2914be27.chunk.css"
+  },
+  {
+    "revision": "0c3145ae9fd98f88c520",
+    "url": "/pomodoro/static/js/2.e2132282.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pomodoro/static/js/2.e2132282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "722810dbd8b844450306",
+    "url": "/pomodoro/static/js/main.27680c4a.chunk.js"
+  },
+  {
+    "revision": "d13b1f1c4e2fdd32784a",
+    "url": "/pomodoro/static/js/runtime-main.c0f3f903.js"
+  }
+]);
